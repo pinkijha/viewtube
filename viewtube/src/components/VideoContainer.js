e464a1ae0349b5b14eaabcf6b65ed2a650a1a1e1
Pinki Jha <pinkijha1310@gmail.com>
2024-11-08 15:46:31 +0530

    add videoCard components and solve the 403 forbidden error and ue useState in app

--- viewtube/src/components/VideoContainer.js
+++ viewtube/src/components/VideoContainer.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { YOUTUBE_VIDEOS_API } from '../utils/Constant'
+import VideoCards from './VideoCards';
 
 const VideoContainer = () => {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() =>{
+    getVideos();
+
+  }, []);
+
+  const getVideos = async () =>{
+    const data = await fetch(YOUTUBE_VIDEOS_API);
+    const json = await data.json();
+    // console.log(json.items);
+    setVideos(json.items);
+  }
+
+
   return (
     <div>
-      ideoContainer
+      <VideoCards info={videos[0]} />
     </div>
   )
 }

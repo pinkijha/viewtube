86eb9be0a2cfbf95efcdeef5c8edfed6d46219c0
Pinki Jha <pinkijha1310@gmail.com>
2024-11-08 19:25:02 +0530

    Add <Link> tag to the VideoCard inside VideoContainer.js

--- viewtube/src/components/VideoContainer.js
+++ viewtube/src/components/VideoContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { YOUTUBE_VIDEOS_API } from '../utils/Constant'
 import VideoCards from './VideoCards';
+import { Link } from 'react-router-dom';
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
@@ -21,7 +22,7 @@ const VideoContainer = () => {
   return (
     <div className='flex flex-wrap'>
       {videos.map(video =>  (
-        <VideoCards key={video.id} info= {video} />
+       <Link to={'/watch?v='+ video.id} > <VideoCards key={video.id} info= {video} /> </Link>
         ))}
       
     </div>

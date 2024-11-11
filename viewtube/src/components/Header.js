48a9de76b7a4f342526b658a3ab4c0b37eecf5e7
Pinki Jha <pinkijha1310@gmail.com>
2024-11-11 17:42:52 +0530

    add the SearchSuggestion Api by using Debouncing concept

--- viewtube/src/components/Header.js
+++ viewtube/src/components/Header.js
@@ -1,14 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/appSlice';
+import { YOUTUBE_SEARCH_API } from '../utils/Constant';
 
 
 const Header = () => {
-  const dispatch = useDispatch();
+  const [searchQuery , setSearchQuery] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  
 
+  useEffect(() => {
+
+    const timer = setTimeout(() => getSearchSuggestion(), 200);
+
+    return () =>{
+      clearTimeout(timer)
+    };
+    
+
+  }, [searchQuery]);
+
+
+  // Make en Api Call After each key press
+  const getSearchSuggestion = async() => {
+    
+    //Api Call
+    console.log("API CALL - "+searchQuery);
+
+    const data = await fetch(YOUTUBE_SEARCH_API+ searchQuery);
+    const json = await data.json();
+    setSuggestions(json[1]);
+    // console.log(json[1])
+  }
+
+
+//ToggleMenu
+  const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch (toggleMenu());
   };
+
+
 
   return ( 
 
@@ -21,9 +54,24 @@ const Header = () => {
       </div>
 
       <div className='col-span-10'>
-        <input className='p-2 ml-[200px] w-1/2 border border-gray-400 rounded-l-full ' placeholder='Search' type='text' />
+       <div>
+       <input className='px-5 p-2 ml-[200px] w-1/2 border border-gray-400 rounded-l-full '
+         placeholder='Search' type='text'
+         value={searchQuery}
+         onChange={(e) => setSearchQuery(e.target.value)}
+         onFocus={() => setShowSuggestions(true)}
+         onBlur={() => setShowSuggestions(false)} />
         <button className='px-5 py-2  border border-gray-400 rounded-r-full bg-gray-100 '>🔍</button>
       </div>
+      {showSuggestions && (
+        <div className='ml-[200px] absolute bg-white py-2 px-2 w-[37rem] shadow-lg border-gray-100 rounded-lg'>
+        <ul>
+          {suggestions.map((s) => <li key={s} className='py-2 px-3 shadow-sm hover:bg-gray-100'>🔍 {s}</li>)}
+
+        </ul>
+      </div>)
+      }
+       </div>
 
       <div>
         <img className='h-8 col-span-1' alt='user-icon'

b5819375cfbdc36a1ab81390f145796b39a129c9
Pinki Jha <pinkijha1310@gmail.com>
2024-11-12 14:50:37 +0530

    add CommentContainer Component and add some comment related data into it

---
+++ viewtube/src/components/CommentsContainer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const commentData = [
+    {
+        name: "Pinki jha",
+        text: "Awesome! video Content, It was Greatest Video ever. Love from me!!",
+        replies: [
+
+        ]
+    },
+    {
+       name: "Pinki jha",
+        text: "Awesome! video Content, It was Greatest Video ever. Love from me!!",
+        replies: [
+            
+        ]
+    },
+    {
+        name: "Pinki jha",
+        text: "Awesome! video Content, It was Greatest Video ever. Love from me!!",
+        replies: [
+            
+        ]
+    },
+    {
+        name: "Pinki jha",
+        text: "Awesome! video Content, It was Greatest Video ever. Love from me!!",
+        replies: [
+            
+        ]
+    }
+    ]
+
+const CommentsContainer = () => {
+  return (
+    <div className='m-5 p-2'>
+      <h1 className='text-2xl font-bold'>Comments</h1>
+    </div>
+  )
+}
+
+export default CommentsContainer

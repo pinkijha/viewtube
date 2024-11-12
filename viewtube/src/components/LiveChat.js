73a329d5b720441ca492b7fa6cfdb067ab7ab6b5
Pinki Jha <pinkijha1310@gmail.com>
2024-11-13 01:04:03 +0530

    working on live Chat create Component of liveChat and add some tailwind css on it

---
+++ viewtube/src/components/LiveChat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LiveChat = () => {
+  return (
+    <div className=' h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg '>
+      <h1>Live Chat</h1>
+    </div>
+  )
+}
+
+export default LiveChat
--- viewtube/src/components/WatchPage.js
+++ viewtube/src/components/WatchPage.js
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utils/appSlice';
 import { useSearchParams } from 'react-router-dom'
 import CommentsContainer from './CommentsContainer';
+import LiveChat from './LiveChat';
 
 const WatchPage = () => {
     const  [searchParams, setSearchParams] = useSearchParams();
@@ -13,13 +14,20 @@ const WatchPage = () => {
         dispatch(closeMenu());
     },[dispatch])
   return (
-    <div className='flex flex-col ml-[100px]'>
-    <div className='px-5 '>
+    <div className='flex flex-col ml-[100px]  w-full'>
+    <div className='px-5 flex '>
+      <div>
       <iframe className='rounded-xl' width="900" height="500" 
       src={"https://www.youtube.com/embed/"+ searchParams.get('v')} title="YouTube video player" 
       frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
       referrerPolicy="strict-origin-when-cross-origin" allowFullScreen>        
       </iframe>
+      </div>
+
+      <div className='w-full'>
+        <LiveChat/>
+      </div>
+
     </div>
     <CommentsContainer/>
     </div>

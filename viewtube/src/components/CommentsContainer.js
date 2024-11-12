a13794e3d1d2f4bf9203ac7f86b5ffa6fea6c3a4
Pinki Jha <pinkijha1310@gmail.com>
2024-11-12 15:36:20 +0530

    try to add more comment using destructuring

--- viewtube/src/components/CommentsContainer.js
+++ viewtube/src/components/CommentsContainer.js
@@ -125,7 +125,7 @@ const commentData = [
 const Comment = ({data}) =>{
     const {name, text, replies} = data;
     return (
-        <div className='flex shadow-sm bg-gray-100 p-2'>
+        <div className='flex shadow-sm bg-gray-100 p-2 rounded-lg my-2'>
             <img  className='w-10 h-10'
             alt='user-img' src='https://cdn.pixabay.com/photo/2014/04/02/17/07/user-307993_640.png' />
             <div className='px-3'>
@@ -134,14 +134,23 @@ const Comment = ({data}) =>{
             </div>
         </div>
     )
-}    
+};
+
+const CommentList = ({comments}) =>{
+    return comments.map((comment, index) =>(
+        // Dont Use index as a key
+        <Comment key={index} data={comment} />
+    ));
+}
+
+
 const CommentsContainer = () => {
   return (
-    <div className='m-5 p-2'>
+    <div className='m-5 p-3'>
       <h1 className='text-2xl font-bold p-2'>Comments</h1>
-      <Comment data={commentData[0]} />
+      <CommentList comments={commentData} />
     </div>
   )
 }
 
-export default CommentsContainer
+export default CommentsContainer;

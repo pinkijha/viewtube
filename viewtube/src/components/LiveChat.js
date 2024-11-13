7fc1f258403c7acc78e9c5d0a04b1b1dbdd1da1c
Pinki Jha <pinkijha1310@gmail.com>
2024-11-13 17:14:32 +0530

    add another slice into our store and dispatch action and useSlector to add livechat data

--- viewtube/src/components/LiveChat.js
+++ viewtube/src/components/LiveChat.js
@@ -1,20 +1,36 @@
 import React, { useEffect } from 'react'
-import ChatMessages from './ChatMessages'
+import ChatMessages from './ChatMessages';
+import { useDispatch, useSelector } from "react-redux"
+import { addMessage } from '../utils/chatSlice';
 
 const LiveChat = () => {
+    const dispatch = useDispatch();
+    const ChatMessage = useSelector((store) => store.chat.messages)
     useEffect(() => {
         const i = setInterval(()=>{
             //API Polling
-            console.log("API Polling");
+            // console.log("API Polling");
+            dispatch(addMessage(
+                {
+                    name: 'Pinki Jha',
+                    message: "hello!🙏 good Afternoon"
+                }
+            ))
+
 
         },2000);
 
         return () => clearInterval(i);
     },[]);
-    
+
   return (
     <div className='overflow-y-scroll h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg '>
-      <ChatMessages name= 'Pinki Jha' message='Hello🙏' />
+      {
+        // dont use index as a key
+       
+      ChatMessage.map((c, index) => {
+        return <ChatMessages key={index} name= {c.name} message={c.message} />
+      })}
     </div>
   )
 }

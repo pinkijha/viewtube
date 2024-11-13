6b6e50be18db90ed0e35a4ac0ce54040280b4b72
Pinki Jha <pinkijha1310@gmail.com>
2024-11-13 16:33:28 +0530

    use useEffect in liveChat

--- viewtube/src/components/LiveChat.js
+++ viewtube/src/components/LiveChat.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ChatMessages from './ChatMessages'
 
 const LiveChat = () => {
+    useEffect(() => {
+        const i = setInterval(()=>{
+            //API Polling
+            console.log("API Polling");
+
+        },2000);
+
+        return () => clearInterval(i);
+    },[]);
+    
   return (
     <div className='overflow-y-scroll h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg '>
-      <ChatMessages name= 'Pinki Jha' message='Hello🙏' />
-      <ChatMessages name= 'Pinki Jha' message='Hello🙏' />
       <ChatMessages name= 'Pinki Jha' message='Hello🙏' />
     </div>
   )
